1a4f9ff7d128fcdedea6ae667cdedad630d5adbf
Baran Taşcı <barantasci44@gmail.com>
2026-04-06 20:19:14 +0300

    add main entry point (main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/fonts/Gilroy-Regular.ttf";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./components/App/App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { setupAxios } from "./utils/api.js";
+setupAxios(store);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
-)
+);
